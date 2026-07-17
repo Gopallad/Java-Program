d2c62bdeaec120dbe437d4307e80f8546f354d46
Gopal Narayan Lad <gopallad555@gmail.com>
2026-07-17 23:24:44 +0530

    Add files via upload

---
+++ Array/CountEvenOdd.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class CountEvenOdd {
+    static void main(String[] args) {
+
+        int[] arr = {10,15,20,25,30,65,40};
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] % 2 == 0)
+                evenCount++;
+            else
+                oddCount++;
+        }
+        System.out.println("Even Count ="+evenCount);
+        System.out.println("Odd Count = "+oddCount);
+    }
+}
