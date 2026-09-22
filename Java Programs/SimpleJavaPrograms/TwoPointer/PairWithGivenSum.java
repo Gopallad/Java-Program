8f17d2dee451c087d676ce62978a689d4ef3ecbe
Gopal Narayan Lad <gopallad555@gmail.com>
2026-09-22 23:28:31 +0530

    Add files via upload

---
+++ Java Programs/SimpleJavaPrograms/TwoPointer/PairWithGivenSum.java
@@ -0,0 +1,27 @@
+public class PairWithGivenSum {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 6, 8};
+        int target = 10;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                System.out.println("Pair: " + arr[left] + ", " + arr[right]);
+                break;
+            } 
+            else if (sum < target) {
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+    }
+}
