34add0b2daaf5f8530aea875def912cd18dba239
Gopal Narayan Lad <gopallad555@gmail.com>
2026-09-23 23:47:07 +0530

    Add files via upload

---
+++ Java Programs/SimpleJavaPrograms/TwoPointer/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesSortedArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 1, 2, 2, 3, 4, 4};
+
+        int i = 0;
+
+        for (int j = 1; j < arr.length; j++) {
+
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+
+        System.out.println(Arrays.toString(Arrays.copyOf(arr, i + 1)));
+    }
+}
